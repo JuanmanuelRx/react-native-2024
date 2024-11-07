9a58694f283049fe018bba934e0a3bf8eb09b0ee
JuanmanuelRx <juanmanuelcastillovas@gmail.com>
2024-11-07 18:29:03 +0100

    hoy dia el 3 proyecto

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnable] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
+  useEffect(()=>{
+    console.log('Efecto', {enabled})
+    const handlemove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({x: clientX, y: clientY})
+    }
+    if(enabled) {
+      window.addEventListener('pointermove', handlemove)
+    }
+    return () => {
+      window.removeEventListener('pointermove', handlemove)
+    }
+  }, [enabled])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        display: enabled? 'block' : 'none',
+        cursor: 'pointer',
+      }}
+        />
+      <button onClick={() => {setEnable(!enabled)}}>
+        {enabled ? 'Activar' : 'Desactivar'} el puntero
+      </button>
+    </main>
+    
   )
 }
 
