9df159111a1eab0ff1b650aaa11a076a85115136
JuanmanuelRx <juanmanuelcastillovas@gmail.com>
2024-11-08 12:38:12 +0100

    Fin del proyecto 3

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-function App() {
+
+const FollowMouse = () => {
   const [enabled, setEnable] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
-  useEffect(()=>{
-    console.log('Efecto', {enabled})
+  useEffect(() => {
     const handlemove = (event) => {
       const { clientX, clientY } = event
-      setPosition({x: clientX, y: clientY})
+      setPosition({ x: clientX, y: clientY })
     }
     if(enabled) {
       window.addEventListener('pointermove', handlemove)
@@ -17,26 +17,43 @@ function App() {
       window.removeEventListener('pointermove', handlemove)
     }
   }, [enabled])
+
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
   return (
-    <main>
+    <>
       <div style={{
-        position: 'absolute',
-        backgroundColor: '#09f',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
-        transform: `translate(${position.x}px, ${position.y}px)`,
-        display: enabled? 'block' : 'none',
-        cursor: 'pointer',
-      }}
-        />
+      position: 'absolute',
+      backgroundColor: '#09f',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+      transform: `translate(${position.x}px, ${position.y}px)`,
+      display: enabled? 'block' : 'none',
+      cursor: 'pointer',
+      }}/>
       <button onClick={() => {setEnable(!enabled)}}>
         {enabled ? 'Activar' : 'Desactivar'} el puntero
       </button>
+    </>
+    
+  )
+}
+
+function App() {
+  const [mounted, setMounted] = useState(true)
+  return (
+    <main>
+      {mounted && <FollowMouse />}
+      <button onClick={() => setMounted(!mounted)}>Desmontar</button>
     </main>
     
   )
